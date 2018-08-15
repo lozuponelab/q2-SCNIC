fbae2ba4ed005913c2bd01abbd929377f14fedc3
shafferm <michael.shaffer@ucdenver.edu>
2018-08-15 12:04:15 +0200

    Added initial tests for _SCNIC_methods.py

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # q2-SCNIC
 
-A package for accessing the primary methods of [SCNIC](https://www.github.com/shafferm/SCNIC)via QIIME2.
+A package for accessing the primary methods of [SCNIC](https://www.github.com/shafferm/SCNIC) via QIIME2.
---
+++ tests/test_SCNIC_methods.py
@@ -0,0 +1,70 @@
+import pytest
+
+from biom import load_table
+import os
+import pandas as pd
+from numpy.testing import assert_allclose
+
+from q2_SCNIC._SCNIC_methods import sparcc_filter, calculate_correlations, build_correlation_network_r, \
+                                    build_correlation_network_p, make_modules_on_correlation_table
+
+
+@pytest.fixture()
+def data_path():
+    return os.path.join(os.path.realpath(os.path.dirname(__file__)), 'data')
+
+
+@pytest.fixture()
+def table(data_path):
+    return load_table(os.path.join(data_path, 'fake_data.biom'))
+
+
+def test_sparcc_filter(table):
+    table_filt = sparcc_filter(table)
+    assert table_filt.shape == table.shape
+
+
+@pytest.fixture()
+def correls_spar(data_path):
+    correls = pd.read_table(os.path.join(data_path, 'fake_correls_spar.txt'), index_col=(0, 1), sep='\t',
+                            dtype={'feature1': str, 'feature2': str})
+    new_index = pd.MultiIndex.from_tuples([(str(i), str(j)) for i, j in correls.index])
+    correls.index = new_index
+    return correls
+
+
+@pytest.fixture()
+def correls_pear(data_path):
+    correls = pd.read_table(os.path.join(data_path, 'fake_correls_pear.txt'), index_col=(0, 1), sep='\t',
+                            dtype={'feature1': str, 'feature2': str})
+    new_index = pd.MultiIndex.from_tuples([(str(i), str(j)) for i, j in correls.index])
+    correls.index = new_index
+    return correls
+
+
+def test_calculate_correlations(table, correls_spar, correls_pear):
+    # TODO: generate results for pearman test
+    test_correls_pear = calculate_correlations(table, method='pearson')
+    assert_allclose(test_correls_pear.values, correls_pear.values, atol=.05)
+    test_correls_spar = calculate_correlations(table, method='sparcc')
+    assert_allclose(test_correls_spar.values, correls_spar.values, atol=.05)
+
+
+def test_build_correlation_network_r(correls_spar):
+    test_net = build_correlation_network_r(correls_spar, min_val=.5)
+    assert len(test_net.nodes) == 4
+    assert len(test_net.edges) == 2
+
+
+def test_build_correlation_network_p(correls_pear):
+    test_net = build_correlation_network_p(correls_pear, max_val=1e-10)
+    assert len(test_net.nodes) == 18
+    assert len(test_net.edges) == 18
+
+
+def test_make_modules_on_correlation_table(correls_spar, table):
+    test_collapsed, test_net = make_modules_on_correlation_table(correls_spar, table, min_r=.5)
+    assert test_collapsed.shape == (49, 200)
+    assert table.sum() == test_collapsed.sum()
+    assert len(test_net.nodes) == 2
+    assert len(test_net.edges) == 1
