cfd68dd186371a8bcccb3bd45ce50edcdba0ad34
shafferm <michael.shaffer@ucdenver.edu>
2018-08-30 17:48:58 -0600

    Add test of ModuleMembershipTSVFormat associated format

--- tests/test_format.py
+++ tests/test_format.py
@@ -1,7 +1,7 @@
 import pytest
 from os import path
 
-from  q2_SCNIC._format import PairwiseFeatureDataFormat, GraphModelingLanguageFormat
+from  q2_SCNIC._format import PairwiseFeatureDataFormat, GraphModelingLanguageFormat, ModuleMembershipTSVFormat
 
 
 @pytest.fixture()
@@ -9,23 +9,19 @@ def data_path():
     return path.join(path.realpath(path.dirname(__file__)), 'data')
 
 
-@pytest.fixture()
-def correls_file_path(data_path):
-    return path.join(data_path, 'fake_correls_spar.txt')
-
-
-def test_PairwiseFeatureDataFormat(correls_file_path):
-    format = PairwiseFeatureDataFormat(correls_file_path, mode='r')
+def test_PairwiseFeatureDataFormat(data_path):
+    format = PairwiseFeatureDataFormat(path.join(data_path, 'fake_correls_spar.txt'), mode='r')
     format.validate('min')
     format.validate('max')
 
 
-@pytest.fixture()
-def gml_file_path(data_path):
-    return path.join(data_path, 'fake_net.gml')
+def test_GraphModelingLanguageFormat(data_path):
+    format = GraphModelingLanguageFormat(path.join(data_path, 'fake_net.gml'), mode='r')
+    format.validate('min')
+    format.validate('max')
 
 
-def test_GraphModelingLanguageFormat(gml_file_path):
-    format = GraphModelingLanguageFormat(gml_file_path, mode='r')
+def test_ModuleMembershipTSVFormat(data_path):
+    format = ModuleMembershipTSVFormat(path.join(data_path, 'fake_membership.txt'), mode='r')
     format.validate('min')
     format.validate('max')
