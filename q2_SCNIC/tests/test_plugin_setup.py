8b4bfcf980a3b712dde2619bc9fd72cfb884697d
John Sterrett <sterrettJD@protonmail.com>
2025-05-29 14:31:28 -0600

    add headers

--- q2_SCNIC/tests/test_plugin_setup.py
+++ q2_SCNIC/tests/test_plugin_setup.py
@@ -1,3 +1,11 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2016-2025, QIIME 2 development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
+
 from q2_SCNIC.plugin_setup import plugin
 
 def test_setup():
